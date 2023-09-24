45c7e92034927ca9dd200823fbb2d7abd309c342
Alex <stukalovsaleksandrs@gmail.com>
2023-09-24 19:56:58 +0300

    tmp

--- main.go
+++ main.go
@@ -40,19 +40,59 @@ func base10ToBase2(num float64) (out string) {
 	// Add the residue to the slice straight away
 
 	// Whole part
-	for wp := math.Floor(num); wp != 0; {
-		wp = wp / 2
-		if wp-math.Floor(wp) > 0 {
+	for n := math.Floor(num); n != 0; {
+		n = n / 2
+		if n-math.Floor(n) > 0 {
 			out = "1" + out
 		} else {
 			out = "0" + out
 		}
-		wp = math.Floor(wp)
+		n = math.Floor(n)
+	}
+	out += "."
+
+	// Fractional part
+	// * 2 and cut the whole part until the fractional part is either 0 or repeats
+
+	// TODO: rewrite using O(log(n)) search
+	var h []float64 // history
+	// Returns whether a is present in h
+	repeated := func(a float64) bool {
+		for _, x := range h {
+			if x == a {
+				return true
+			}
+		}
+		return false
+	}
+
+	for n := num - math.Floor(num); n != 0 && !repeated(n); {
+		if n = 2 * n; n > 1 {
+			out += "1"
+			n -= 1.0
+		} else {
+			out += "0"
+		}
 	}
 	return out
 }
 
 func main() {
-	a := base10ToBase2(22.0)
+	a := base10ToBase2(0.2)
 	fmt.Println(a)
+}
+
+type Heap []int
+
+// Source := https://github.com/34thSchool/heap-1
+// Push pushes the element x onto the heap.
+// The complexity is O(log n) where n = h.Len().
+func (h *Heap) Push(x int) {
+	// Add new element to the end of the Heap
+	// compare new element's value with its parent
+	// if new element < parent -> swap them
+	*h = Heap(append(*h, x))
+	for i := len(*h) - 1; i > 0 && (*h)[i] < (*h)[(i-1)/2]; i = (i - 1) / 2 {
+		(*h)[i], (*h)[(i-1)/2] = (*h)[(i-1)/2], (*h)[i]
+	}
 }
