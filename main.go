688ebbca177e1a45ccf9cd659fb9ffae1210ac75
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-09-26 15:11:13 +0000

    Finished Base2ToBase10()

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 	"strconv"
 )
 
+// Accuracy threshold
+var e float64 = 1e-3
+
 func base2ToBase10(num string) (float64, error) {
 	if !regexp.MustCompile(`^-?[01]*[.]?[01]+$`).MatchString(num) {
 		return 0, errors.New("IncorrectInput")
@@ -35,12 +38,8 @@ func base2ToBase10(num string) (float64, error) {
 }
 
 func base10ToBase2(num float64) (out string) {
-	// Get whole and decimal parts
-	// Divide whole part by 2 until it's 0
-	// Add the residue to the slice straight away
-
 	// Whole part
-	for n := math.Floor(num); n != 0; {
+	for n := math.Floor(math.Abs(num)); n != 0; {
 		n = n / 2
 		if n-math.Floor(n) > 0 {
 			out = "1" + out
@@ -49,25 +48,42 @@ func base10ToBase2(num float64) (out string) {
 		}
 		n = math.Floor(n)
 	}
-	out += "."
+
+	// Handling negative values
+	if num < 0 {
+		out = "-" + out
+		num = math.Abs(num)
+	}
+
+	// returns whether a ~ b within the threshold e
+	nearlyEqual := func(a, b float64) bool {
+		return math.Abs(a-b) < e
+	}
+	if nearlyEqual(num-math.Floor(num), 0) { // Fractional part is abscent
+		return out
+	}
 
 	// Fractional part
-	// * 2 and cut the whole part until the fractional part is either 0 or repeats
+	out += "."
 
 	// TODO: rewrite using O(log(n)) search
 	var h []float64 // history
-	// Returns whether a is present in h
-	repeated := func(a float64) bool {
-		for _, x := range h {
-			if x == a {
+	// Returns whether x is present in h
+	repeated := func(x float64) bool {
+		for i := range h {
+			if nearlyEqual(x, h[i]) {
 				return true
 			}
 		}
 		return false
 	}
-
+	// Returns whether a > b within the constant threshold e
+	greaterEqual := func(a, b float64) bool {
+		return a-b > e || nearlyEqual(a, b)
+	}
 	for n := num - math.Floor(num); n != 0 && !repeated(n); {
-		if n = 2 * n; n > 1 {
+		h = append(h, n)
+		if n *= 2; greaterEqual(n, 1) {
 			out += "1"
 			n -= 1.0
 		} else {
@@ -78,21 +94,5 @@ func base10ToBase2(num float64) (out string) {
 }
 
 func main() {
-	a := base10ToBase2(0.2)
-	fmt.Println(a)
-}
-
-type Heap []int
-
-// Source := https://github.com/34thSchool/heap-1
-// Push pushes the element x onto the heap.
-// The complexity is O(log n) where n = h.Len().
-func (h *Heap) Push(x int) {
-	// Add new element to the end of the Heap
-	// compare new element's value with its parent
-	// if new element < parent -> swap them
-	*h = Heap(append(*h, x))
-	for i := len(*h) - 1; i > 0 && (*h)[i] < (*h)[(i-1)/2]; i = (i - 1) / 2 {
-		(*h)[i], (*h)[(i-1)/2] = (*h)[(i-1)/2], (*h)[i]
-	}
+	fmt.Println(base10ToBase2(-3.125))
 }
